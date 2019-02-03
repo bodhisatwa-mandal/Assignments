7ac759a0dddd55dbe8599d0bd7a91bdb34bbdfdc
Bodhisatwa Mandal <bodhisatwam@gmail.com>
2019-02-04 00:37:29 +0530

    4/02/2019

--- 3rdYr_2ndSem/Network/Flow_Sender.py
+++ 3rdYr_2ndSem/Network/Flow_Sender.py
@@ -1,2 +1,6 @@
 import socket
 #%%
+class Sender:
+    def __init__(self, host=socket.gethostname(), sender_port=1234):
+        self.sender_client = socket.socket()
+        self.sender_client.connect((host, sender_port))
