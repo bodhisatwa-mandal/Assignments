1de71c7c332b344a14ad20a21e528b1c2921866b
bodhisatwa-mandal <bodhisatwam@gmail.com>
2019-02-21 00:39:34 +0530

    20/02/19

--- 3rdYr_2ndSem/Network/Flow_Selective_Repeat_Arq_Sender.py
+++ 3rdYr_2ndSem/Network/Flow_Selective_Repeat_Arq_Sender.py
@@ -9,9 +9,10 @@ class Sender:
         self.sender_client.connect((host, sender_port))
         self.sender_client.settimeout(2)
         self.timer_threshold =3
+        self.frame_len = frame_len
         print("Sender Connected with Channel")
 
-        self.sent_time = [0]*frame_len
+        self.sent_timer = [0]*frame_len
         self.sender_window = [False]*frame_len
         self.ack_id = 0
 
@@ -29,7 +30,7 @@ class Sender:
             if ack is None:
                 continue
             if int(ack) >= self.ack_id:
-                recieved_id = int(data)
+                recieved_id = int(ack)
                 self.sender_window[recieved_id%self.frame_len] = True
                 self.sent_timer[recieved_id%self.frame_len] = -1
                 if not False in self.sender_window:
@@ -39,19 +40,19 @@ class Sender:
                 
     def send(self):
         while True:
-            if self.ack_id = len(self.frames)-1:
+            if self.ack_id == len(self.frames)-1:
                 self.sender_client.send(self.frames[self.ack_id].encode())
                 break
-            else
+            else:
                 for send_id in range(self.ack_id, self.ack_id+self.frame_len, 1):
-                    if self.sent_timer[send_id] == 0:
+                    if self.sent_timer[send_id%self.frame_len] == 0:
                         print("Sending Frame : ",self.frames[send_id])
-                        self.sender_client.send(self.frames[self.send_id].encode())
-                        self.sent_timer[send_id] = time.time()
-                    elif self.sent_timer[send_id]!=-1 and (time.time()-self.sent_time[send_id]>self.timer_threshold)
+                        self.sender_client.send(self.frames[send_id].encode())
+                        self.sent_timer[send_id%self.frame_len] = time.time()
+                    elif self.sent_timer[send_id%self.frame_len]!=-1 and (time.time()-self.sent_timer[send_id%self.frame_len]>self.timer_threshold):
                         print("Resending Frame : ",self.frames[send_id])
-                        self.sender_client.send(self.frames[self.send_id].encode())
-                        self.sent_timer[send_id] = time.time()
+                        self.sender_client.send(self.frames[send_id].encode())
+                        self.sent_timer[send_id%self.frame_len] = time.time()
 
     def drive(self):
         thread_1 = threading.Thread(target=self.send, args=())
