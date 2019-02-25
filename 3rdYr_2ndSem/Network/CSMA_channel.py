29081fe1fbc8b42f60192960d97cadad57deaa3d
bodhisatwa-mandal <bodhisatwam@gmail.com>
2019-02-25 21:26:37 +0530

    25/02/19

--- 3rdYr_2ndSem/Network/CSMA_channel.py
+++ 3rdYr_2ndSem/Network/CSMA_channel.py
@@ -1,33 +1,94 @@
 import sys
 import socket
+import threading
 #%%
 class Channel:
-    def __init__(self, num_nodes=4, host=socket.gethostname(), port=1234):
+    def __init__(self, num_nodes=4, host=socket.gethostname()):
         self.num_nodes = num_nodes
         self.buffer = []
-        self.data_list = []
+
+        self.from_node_list = []
+        self.to_node_list = []
         self.state_list = []
-        self.data_address_list = []
+        self.from_node_address_list = []
+        self.to_node_address_list = []
         self.state_address_list = []
 
-        data_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        data_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        data_socket.bind((host, port))
-        data_socket.listen(num_nodes)
+        self.from_node_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.from_node_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.from_node_socket.bind((host, 1234))
+        self.from_node_socket.settimeout(2)
+        self.from_node_socket.listen(num_nodes)
 
-        state_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        state_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        state_socket.bind((host, port+1))
-        state_socket.listen(num_nodes)
+        self.to_node_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.to_node_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.to_node_socket.bind((host, 1235))
+        self.from_node_socket.listen(num_nodes)
 
-        while node_index in range(num_nodes):
-            node, node_address = data_socket.accept()
-            self.data_list.append(node)
-            self.data_address_list.append(node_address)
-            print("Node Data Connection", node_index, " Established from : ", self.reciever_address)
-            node, node_address = state_socket.accept()
+        self.state_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.state_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.state_socket.bind((host, 1236))
+        self.from_node_socket.listen(num_nodes)
+
+        for node_index in range(num_nodes):
+
+            node, node_address = self.from_node_socket.accept()
+            self.from_node_list.append(node)
+            self.from_node_address_list.append(node_address)
+            print("Recieve Connection", node_index, " Established from : ", node_address)
+
+            node, node_address = self.to_node_socket.accept()
+            self.to_node_list.append(node)
+            self.to_node_address_list.append(node_address)
+            print("Send Connection", node_index, " Established from : ", node_address)
+
+            node, node_address = self.state_socket.accept()
             self.state_list.append(node)
             self.state_address_list.append(node_address)
-            print("Node State Connection", node_index, " Established from : ", self.reciever_address)
+            print("State Connection", node_index, " Established from : ", node_address)
+
+    def channelStatus(self):
+        if len(self.buffer)>0:
+            for state in self.state_list:
+                self.state.send("Idle".encode())
+        else:
+            for state in self.state_list:
+                self.state.send(buffer[0][0].encode())
+
+    def node_to_buffer(self):
+        for node in self.from_node_list:
+            try:
+                data = node.recv(1024)
+                string = data.decode()
+                self.buffer.append(string)
+            except:
+                pass
+        self.channelStatus()
+
+    def buffer_to_node(self):
+        buff_size = len(buffer)
+        for i in range(buff_size):
+            for node in self.to_node_list:
+                self.node.send(self.buffer[0].encode())
+            del self.buffer[0]
+        self.channelStatus()
+
+    def start_channel(self):
+        thread_1 = threading.Thread(target=self.channelStatus, args=())
+        thread_2 = threading.Thread(target=self.node_to_buffer, args=())
+        thread_3 = threading.Thread(target=self.buffer_to_node, args=())
+        thread_1.start()
+        thread_2.start()
+        thread_3.start()
+        thread_1.join()
+        thread_2.join()
+        thread_3.join()
+#%%
+num_nodes = int(sys.argv[1])
+channel_obj = Channel(num_nodes = num_nodes)
+#%%
+channel_obj.start_channel()
+
+
 
 
--- 3rdYr_2ndSem/Network/CSMA_node.py
+++ 3rdYr_2ndSem/Network/CSMA_node.py
@@ -1,27 +1,82 @@
 import sys
+import time
+import random
+import threading
+import socket
 #%%
 class Node:
-    def __init__(self, node_id, host=socket.gethostname(), port=1234):
+    def __init__(self, node_id, num_nodes, host=socket.gethostname()):
         self.node_id = node_id
         
         self.data_send_socket = socket.socket()
-        self.data_send_socket.connect((host, port))
-
+        self.data_send_socket.connect((host, 1234))
+        
         self.data_recieve_socket = socket.socket()
-        self.data_send_socket.connect((host, port+1))
+        self.data_recieve_socket.connect((host, 1235))
 
         self.state_socket = socket.socket()
-        self.state_socket.connect((host, port+2))
+        self.state_socket.connect((host, 1236))
         
         print("Node", node_id, "Connected to Channel")
         self.can_send = False
 
-    def getChannelInfo(self):
-    	state = self.state_socket.recv(1024).decode()
-    	if state == "True":
-    		self.can_send = True
-    	else:
-    		self.can_send = False
+        self.frames = ['Hello', 'World' , ' ']
+        self.p = 1.0/num_nodes
+
+        self.wait_time = random.random()
+        self.increment_waitimg_time = random.random()
 
     def sendData(self):
+        while True:
+            if self.can_send == True:
+                if random.random() <= self.p:
+                    for frame in self.frames:
+                        print("Sending Frame : ",frame)
+               	        self.sender_client.send((str(self.node_id)+frame).encode())
+                    return
+               	# Value does not satisfy p value
+                else:
+                    print("Waiting for :",self.wait_time)
+                    time.sleep(self.wait_time)
+                    self.wait_time += self.wait_time*self.increment_waitimg_time
+                    self.can_send = False
+            # Channel is Busy
+            else:
+                print("Waiting for :",self.wait_time)
+                time.sleep(self.wait_time)
+                self.wait_time += self.wait_time*self.increment_waitimg_time
+                self.can_send = False
+
+    def recieveData(self):
+        while True:
+            data = self.data_recieve_socket.recv(1024).decode()
+            print("Data Recieved : ", data)
+
+    def getChannelInfo(self):
+        while True:
+            state = self.state_socket.recv(1024).decode()
+            if state == "Idle":
+                self.can_send = True
+            elif int(state) != self.node_id:
+                self.can_send = False
+
+    def drive(self):
+        thread_1 = threading.Thread(target=self.getChannelInfo, args=())
+        thread_2 = threading.Thread(target=self.recieveData, args=())
+        thread_3 = threading.Thread(target=self.sendData, args=())
+        thread_1.start()
+        thread_2.start()
+        thread_3.start()
+        thread_1.join()
+        thread_2.join()
+        thread_3.join()
+#%%
+node_id = int(sys.argv[1])
+num_nodes = int(sys.argv[2])
+node_obj = Node(node_id=node_id, num_nodes=num_nodes)
+#%%
+node.obj.drive()
+
+
+
     	
