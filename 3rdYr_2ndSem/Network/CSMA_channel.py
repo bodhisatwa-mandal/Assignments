821124b9af654b22854d091b59d850cf1dc7f532
bodhisatwa-mandal <bodhisatwam@gmail.com>
2019-02-25 13:15:38 +0530

    25/02/19

--- 3rdYr_2ndSem/Network/CSMA_channel.py
+++ 3rdYr_2ndSem/Network/CSMA_channel.py
@@ -5,16 +5,29 @@ class Channel:
     def __init__(self, num_nodes=4, host=socket.gethostname(), port=1234):
         self.num_nodes = num_nodes
         self.buffer = []
-        self.node_list = []
-        self.node_address_list = []
+        self.data_list = []
+        self.state_list = []
+        self.data_address_list = []
+        self.state_address_list = []
 
-        node_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        node_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        node_socket.bind((host, port))
-        node_socket.listen(num_nodes)
+        data_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        data_socket.bind((host, port))
+        data_socket.listen(num_nodes)
+
+        state_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        state_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        state_socket.bind((host, port+1))
+        state_socket.listen(num_nodes)
 
         while node_index in range(num_nodes):
-            node, node_address = node_socket.accept()
-            node_list.append(node)
-            node_address_list.append(node_address)
-            print("Reciever Connection", node_index, " Established from : ", self.reciever_address)
+            node, node_address = data_socket.accept()
+            self.data_list.append(node)
+            self.data_address_list.append(node_address)
+            print("Node Data Connection", node_index, " Established from : ", self.reciever_address)
+            node, node_address = state_socket.accept()
+            self.state_list.append(node)
+            self.state_address_list.append(node_address)
+            print("Node State Connection", node_index, " Established from : ", self.reciever_address)
+
+
--- 3rdYr_2ndSem/Network/CSMA_node.py
+++ 3rdYr_2ndSem/Network/CSMA_node.py
@@ -1,5 +1,27 @@
 import sys
 #%%
 class Node:
-    def __init__(self, node_id):
+    def __init__(self, node_id, host=socket.gethostname(), port=1234):
         self.node_id = node_id
+        
+        self.data_send_socket = socket.socket()
+        self.data_send_socket.connect((host, port))
+
+        self.data_recieve_socket = socket.socket()
+        self.data_send_socket.connect((host, port+1))
+
+        self.state_socket = socket.socket()
+        self.state_socket.connect((host, port+2))
+        
+        print("Node", node_id, "Connected to Channel")
+        self.can_send = False
+
+    def getChannelInfo(self):
+    	state = self.state_socket.recv(1024).decode()
+    	if state == "True":
+    		self.can_send = True
+    	else:
+    		self.can_send = False
+
+    def sendData(self):
+    	
