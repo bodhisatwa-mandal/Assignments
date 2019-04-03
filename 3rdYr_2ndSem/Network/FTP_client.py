0d7c5ae617806221b0a5e173ae96f87c731cf839
Bodhisatwa Mandal <bodhisatwam@gmail.com>
2019-04-03 20:14:37 +0530

    03/04/19

---
+++ 3rdYr_2ndSem/Network/FTP_client.py
@@ -0,0 +1,22 @@
+import socket               # Import socket module
+
+print("Enter file name")
+filename = str(input())
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                 # Reserve a port for your service.
+
+s.connect((host, port))
+
+f = open(filename,'rb')
+print('Sending Started')
+l = f.read(1024)
+while (l):
+    print('Sending')
+    s.send(l)
+    l = f.read(1024)
+f.close()
+print("Done Sending")
+s.shutdown(socket.SHUT_WR)
+#s.close()                     # Close the socket when done
