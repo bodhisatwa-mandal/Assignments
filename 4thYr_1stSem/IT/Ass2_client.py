ada6bfa09a765b3b629df5e775cf99e823f1bfe4
Bodhisatwa Mandal <bodhisatwam@gmail.com>
2019-08-28 09:36:35 +0530

    28/08/19

--- 4thYr_1stSem/IT/Ass2_client.py
+++ 4thYr_1stSem/IT/Ass2_client.py
@@ -11,6 +11,15 @@ class Client:
 
 		self.host = host
 		self.port = port+pwd
+		self.url = 'ws://'+host+':'+self.port
+
+	async def send(mesg):
+	    async with websockets.connect(self.url) as websocket:
+	        await websocket.send(mesg)
+
+	async def recv():
+	    async with websockets.connect(self.url) as websocket:
+	        return await websocket.recv()
 
 	def main(self):
 
@@ -18,43 +27,43 @@ class Client:
 		while i < len(self.initial_cmd):
 			#print(i, self.initial_cmd[i])
 			if self.initial_cmd[i] == 'exit':
-				self.sock.send('exit'.encode())
+				self.send('exit')
 				print("Exiting")
 				return
 			if self.initial_cmd[i] == 'get':
 				mesg = self.initial_cmd[i]+" "+self.initial_cmd[i+1]
-				self.sock.send(mesg.encode())
+				self.send(mesg)
 				time.sleep(1)
 				print("Query sent : "+mesg)
-				mesg = self.sock.recv(self.buffer).decode()
+				mesg = self.recv(self.buffer)
 				print("Received : ",mesg)
 				time.sleep(1)
 				i += 2
 			elif self.initial_cmd[i] == 'put':
 				mesg = self.initial_cmd[i]+" "+self.initial_cmd[i+1]+" "+self.initial_cmd[i+2]
 				time.sleep(1)
-				self.sock.send(mesg.encode())
+				self.send(mesg)
 				print("Value put successful")
 				i+=3
 
 		while True:
 			cmd = input().split(" ")
 			if cmd[0] == 'exit':
-				self.sock.send('exit'.encode())
+				self.send('exit')
 				print("Exiting")
 				return
 			if cmd[0] == 'get':
 				mesg = cmd[0]+" "+cmd[1]
-				self.sock.send(mesg.encode())
+				self.send(mesg)
 				time.sleep(1)
 				print("Query sent : "+mesg)
-				mesg = self.sock.recv(self.buffer).decode()
+				mesg = self.recv(self.buffer)
 				print("Received : ",mesg)
 				time.sleep(1)
 			elif cmd[0] == 'put':
 				mesg = cmd[0]+" "+cmd[1]+" "+cmd[2]
 				time.sleep(1)
-				self.sock.send(mesg.encode())
+				self.send(mesg)
 				print("Value put successful")
 	
 initial_cmd = sys.argv
